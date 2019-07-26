3f4e5a0266eabeffc9b235bafbe16068dbee696c
CURRENDAS\h.milecki <hubert.milecki@currenda.pl>
2019-07-26 14:35:43 +0200

    Initial Commit

---
+++ src/main/java/pl/currenda/eac/web/EacWebViewerApplication.java
@@ -0,0 +1,13 @@
+package pl.currenda.eac.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EacWebViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EacWebViewerApplication.class, args);
+	}
+
+}
